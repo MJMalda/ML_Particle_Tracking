7405fce3bbda6e76c4df1aa56693ab1da827a0e0
MJMalda <mjm016@lvc.edu>
2022-03-01 15:51:10 -0500

    'Updates'

--- data_formatter_csv.py
+++ data_formatter_csv.py
@@ -5,26 +5,71 @@ import os
 # print(chardet.detect(open('Training_data/training_0_201.csv', 'rb').read())['encoding'])
 
 def inputs(training_file):
-    df = pd.read_csv(training_file, dtype={'PRI_jet_num':'int8'},nrows=10000)
+    df = pd.read_csv(training_file, dtype={'PRI_jet_num':'int8'},nrows=200)
     # df = df.iloc[:50000]
-    labels = df['Label'].to_numpy()
-    label = []
+    # labels = df['Label'].to_numpy()
+    # label = []
+    # for i in range(len(labels)):
+    #     if labels[i] == 's':
+    #         label.append(0)
+    #     elif labels[i] == 'b':
+    #         label.append(1)
+    # label = np.array(label)
+    # data = df.drop(['Weight','Label'], axis=1)
+    data=df.drop(['EventId'], axis=1)
+    label_0 = data['Label'].loc[data['PRI_jet_num']==0]
+    weight_0 = data['Weight'].loc[data['PRI_jet_num']==0]
+    labels = label_0.to_numpy()
+    _label_0 = []
     for i in range(len(labels)):
         if labels[i] == 's':
-            label.append(0)
+            _label_0.append(0)
         elif labels[i] == 'b':
-            label.append(1)
-    label = np.array(label)
-    # data = df.drop(['Weight','Label'], axis=1)
-    data=df.drop(['EventId','Label'], axis=1)
-    jugular = []
-    for i in range(len(data)):
-        list = [data.iloc[i][j] for j in range(len(data.iloc[i]))]
-        jugular.append(list)
-    jugular = np.array(jugular)
+            _label_0.append(1)
+    label_0 = np.array(_label_0)
+    label_1 = data['Label'].loc[data['PRI_jet_num']==1]
+    weight_1 = data['Weight'].loc[data['PRI_jet_num']==1]
+    labels = label_1.to_numpy()
+    _label_1 = []
+    for i in range(len(labels)):
+        if labels[i] == 's':
+            _label_1.append(0)
+        elif labels[i] == 'b':
+            _label_1.append(1)
+    label_1 = np.array(_label_1)
+    label_23 = data['Label'].loc[data['PRI_jet_num']>=2]
+    weight_23 = data['Weight'].loc[data['PRI_jet_num']>=2]
+    labels = label_23.to_numpy()
+    _label_23 = []
+    for i in range(len(labels)):
+        if labels[i] == 's':
+            _label_23.append(0)
+        elif labels[i] == 'b':
+            _label_23.append(1)
+    label_23 = np.array(_label_23)
+    data=data.drop(['Label','Weight'], axis=1)
+    data_0 = (data.loc[data['PRI_jet_num']==0]).drop(['PRI_jet_leading_pt','PRI_jet_leading_eta','PRI_jet_leading_phi','PRI_jet_subleading_pt','PRI_jet_subleading_eta','PRI_jet_subleading_phi','DER_lep_eta_centrality','DER_prodeta_jet_jet','DER_mass_jet_jet','DER_deltaeta_jet_jet'], axis=1)
+    data_1 = (data.loc[data['PRI_jet_num']==1]).drop(['PRI_jet_subleading_pt','PRI_jet_subleading_eta','PRI_jet_subleading_phi','DER_lep_eta_centrality','DER_prodeta_jet_jet','DER_mass_jet_jet','DER_deltaeta_jet_jet'], axis=1)
+    data_23 = data.loc[data['PRI_jet_num']>=2]
+    jet_0 = []
+    jet_1 = []
+    jet_23 = []
+    for i in range(len(data_0)):
+        list = [data_0.iloc[i][j] for j in range(len(data_0.iloc[i]))]
+        jet_0.append(list)
+    jet_0 = np.array(jet_0)
+    for i in range(len(data_1)):
+        list = [data_1.iloc[i][j] for j in range(len(data_1.iloc[i]))]
+        jet_1.append(list)
+    jet_1 = np.array(jet_1)
+    for i in range(len(data_23)):
+        list = [data_23.iloc[i][j] for j in range(len(data_23.iloc[i]))]
+        jet_23.append(list)
+    jet_23 = np.array(jet_23)
     # print(jugular)
     # print(len(jugular))
     # print(label)
-    return jugular, label
+    return jet_0, jet_1, jet_23, label_0, label_1, label_23, weight_0, weight_1, weight_23
 
-# inputs('Training_data/training_0_201.csv')
+# data_0,data_1,data_23,label_0,label_1,label_23=inputs('Training_data/training_0_201.csv')
+# print(label_0)
--- keras_layers.py
+++ keras_layers.py
@@ -3,13 +3,14 @@ import csv
 import data_formatter_csv as dafa
 import numpy as np
 import pandas as pd
+from sb_converter import outputs
 
-training_file = 'Training_data/training.csv'
+training_file = 'Training_data/training_0_201.csv'
 test_file = 'Test_data/test_last_100.csv'
 prediction_file = 'pred_0_201.csv'
 
-data_, label_ = dafa.inputs(training_file)
-test_data,test_labels = dafa.inputs(test_file)
+data_0,data_1,data_23,label_0,label_1,label_23,weight_0,weight_1,weight_23 = dafa.inputs(training_file)
+test_data_0,test_data_1,test_data_23,test_label_0,test_label_1,test_label_23,test_weight_0,test_weight_1,test_weight_23 = dafa.inputs(test_file)
 
 # ams_num = ams.run_AMS(training_file,prediction_file) #how to incorporate?
 def fit_data(data,label):
@@ -18,15 +19,15 @@ def fit_data(data,label):
      from keras.callbacks import EarlyStopping 
 
      model = Sequential()
-     model.add(Dense(64,activation='relu',input_shape=(31,)))
-     model.add(Dense(64,activation='relu',input_shape=(31,)))
-     model.add(Dense(32,activation='relu',input_shape=(31,)))
+     model.add(Dense(30,activation='relu',input_shape=(np.shape(data)[-1],)))
+     model.add(Dense(30,activation='relu',input_shape=(np.shape(data)[-1],)))
+     model.add(Dense(20,activation='relu',input_shape=(np.shape(data)[-1],)))
      model.add(Dense(1,activation='sigmoid'))
 
      early_stopping_monitor = EarlyStopping(patience=5)
 
      model.compile(optimizer='adam',loss='binary_crossentropy', metrics=['accuracy'])
-     model.fit(data,label,validation_split=0.3, epochs=100,callbacks=[early_stopping_monitor])
+     model.fit(data,label,validation_split=0.2, epochs=100,callbacks=[early_stopping_monitor])
      return model
 
 #predicts and formats prediction file
@@ -35,29 +36,73 @@ def make_predictions(test_data_,model):
      pred = []
      for i in range(len(prediction)):
           pred.append(str(int(round(prediction[i][0]))))
-
-     #Writing to prediction file a list of 1's and 0's
-     with open(prediction_file,'w') as f:
-          pred_ = ['_'] + pred #to be replaced with header
-          writer = csv.writer(f)
-          writer.writerows(pred_)
-
-     file = pd.read_csv(prediction_file)
-     file.to_csv(prediction_file, header=['Pred_Label'], index=False)
-
-     from sb_converter import outputs
-     outputs(prediction_file)
      return pred
 
-model_ = fit_data(data_,label_)
-pred = make_predictions(test_data,model_)
+model_0 = fit_data(data_0,label_0)
+pred_0 = make_predictions(test_data_0,model_0)
+model_1 = fit_data(data_1,label_1)
+pred_1 = make_predictions(test_data_1,model_1)
+model_23 = fit_data(data_23,label_23)
+pred_23 = make_predictions(test_data_23,model_23)
+pred_ = []
+for i in pred_0:
+     pred_.append(int(i))
+for i in pred_1:
+     pred_.append(int(i))
+for i in pred_23:
+     pred_.append(int(i))
+
+#Writing to prediction file a list of 1's and 0's
+pred_f = []
+with open(prediction_file,'w') as f:
+     pred_f = ['_'] + pred_ #to be replaced with header
+     # print(pred_f)
+     writer = csv.writer(f)
+     writer.writerows(map(lambda x: [x],pred_f))
+
+file = pd.read_csv(prediction_file)
+file.to_csv(prediction_file, header=['Pred_Label'], index=False)
+outputs(prediction_file)
+
+#Rewriting test file
+test_l = []
+test_w = []
+for i in test_label_0:
+     test_l.append(int(i))
+for i in test_label_1:
+     test_l.append(int(i))
+for i in test_label_23:
+     test_l.append(int(i))
+
+for i in test_weight_0:
+     test_w.append(float(i))
+for i in test_weight_1:
+     test_w.append(float(i))
+for i in test_weight_23:
+     test_w.append(float(i))
+d = {'Label':test_l,'Weight':test_w}
+df = pd.DataFrame(d)
+df.to_csv('generated_test.csv',index=False)
+# with open('generated_test.csv','w') as f:
+#      test_ = ['_'] + test_l #to be replaced with header
+#      writer = csv.writer(f)
+#      writer.writerows(map(lambda x: [x],df))
+
+file = pd.read_csv('generated_test.csv')
+file.to_csv('generated_test.csv', header=['Pred_Label','Weight'], index=False)
+weight = pd.read_csv('generated_test.csv',usecols=['Weight'])
+outputs('generated_test.csv')
+#.to_csv('generated_test.csv', header=['Label','Weight'], index=False)
+
+test = pd.read_csv('generated_test.csv',usecols=['Label'])
+test = test['Label'].to_numpy()
+print(test)
 ams_num = ams.run_AMS(test_file,prediction_file)
+print(f'Percentage of points predicted incorrectly: {sum(abs(np.array(pred_)-np.array(test)))/len(pred_)*100}%')
 print(ams_num) #prints predicted ams vs perfect ams
-# print(pred)
-# print()
-# print(test_labels)
 
-#do we need to remove headers? Figure out if possible to input folder with colums out of order
-#Incorporating AMS
-
-#We have two different input shapes if we create a second dataset that omits all -999's. Then we can have two different models!
+#Do we need to remove headers? Figure out if possible to input folder with colums out of order
+#Incorporating AMS -- Loss function? <-- Maybe some other loss model
+#Seperate models for different jet models -- 3 or 4 different models
+#Descending order in correlation to EventID so that we maintain correct order (Pandas?)
+#Speed up data importing -- designate dtypes?
